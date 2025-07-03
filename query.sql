6f95c6fe72a48074dfbf17a7a0ebc20f553a67eb
robertromans <robertromans@hotmail.it>
2025-07-03 13:41:50 +0200

    Add files via upload

--- query.sql
+++ query.sql
@@ -7,11 +7,11 @@ AND T.citta_arrivo = 'Roma'
 AND C.data = '2025-07-15';
 
 -- 2. Verifica della disponibilità di posti su una corsa
-SELECT M.numero_posti - COUNT(B.id) AS posti_disponibili
+SELECT M.numero_posti - COUNT(B.id_biglietto) AS posti_disponibili
 FROM Corse C
 JOIN Mezzi M ON C.targa_mezzo = M.targa_mezzo
-LEFT JOIN Biglietti B ON B.id_corsa = C.id
-WHERE C.id = 29;
+LEFT JOIN Biglietti B ON B.id_corsa = C.id_corsa
+WHERE C.id_corsa = 29;
 
 -- 3. Storico biglietti acquistati da un passeggero
 SELECT B.id_biglietto, B.data_emissione, T.citta_partenza, T.citta_arrivo, C.data, C.ora_partenza
@@ -23,7 +23,7 @@ WHERE P.email = 'john.doe@yahoo.com'
 ORDER BY C.data DESC;
 
 -- 4. Controllo validità di un biglietto
-SELECT B.id, C.data, C.ora_partenza,
+SELECT B.id_biglietto, C.data, C.ora_partenza,
   CASE 
     WHEN C.data > CURDATE() OR (C.data = CURDATE() AND C.ora_partenza > CURTIME())
     THEN 'Valido'
@@ -37,6 +37,6 @@ WHERE B.id_biglietto = 329;
 SELECT C.id_corsa, C.data, T.citta_partenza, T.citta_arrivo
 FROM Corse C
 JOIN Tratte T ON C.id_tratta = T.id_tratta
-WHERE C.id_mezzo = 'AA052CL'
+WHERE C.targa_mezzo = 'AA052CL'
   AND C.data BETWEEN '2024-08-01' AND '2025-07-31'
 ORDER BY C.data;
